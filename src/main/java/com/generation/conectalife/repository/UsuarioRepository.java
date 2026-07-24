e829774dd7dc5d9abf0881214d7df5a818b21329
kauedota <kauedota@gmail.com>
2026-07-24 11:58:14 -0300

    Implementação da entidade Usuario com atributos nome, email e senha, criação do UsuarioRepository e UsuarioController com CRUD completo

--- src/main/java/com/generation/conectalife/repository/UsuarioRepository.java
+++ src/main/java/com/generation/conectalife/repository/UsuarioRepository.java
@@ -1,5 +1,13 @@
 package com.generation.conectalife.repository;
 
-public interface UsuarioRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.generation.conectalife.model.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+	
+	// Consulta personalizada para buscar um usuário pelo email
+	Usuario findByEmail(String email);
 
 }
