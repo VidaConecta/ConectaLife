cb630a9dafe8a7df7271903c0563b759fc39ad15
Victor Pedro <victorr.pgms@gmail.com>
2026-07-24 09:57:56 -0300

    :tada: commit inicial

---
+++ src/test/java/com/generation/conectalife/ConectalifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.conectalife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConectalifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
