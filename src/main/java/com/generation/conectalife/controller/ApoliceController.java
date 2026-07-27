e0ef8bfa654b279a0cb856e775aa7c84588f775e
Victor Pedro <victorr.pgms@gmail.com>
2026-07-27 12:30:11 -0300

    :sparkles: Correção do código

--- src/main/java/com/generation/conectalife/controller/ApoliceController.java
+++ src/main/java/com/generation/conectalife/controller/ApoliceController.java
@@ -50,18 +50,7 @@ public class ApoliceController {
                 .body(apoliceRepository.save(apolice));
     }
 	
-<<<<<<< HEAD
-	@PutMapping("/{id}")
-=======
-	/*@PutMapping
->>>>>>> a0ac5d62d2a4186fbe217502443d684d9cf90dca
-    public ResponseEntity<Apolice> put(@Valid @RequestBody Apolice apolice) {
-        if (apolice.getId() == null || !apoliceRepository.existsById(apolice.getId())) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        }
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(apoliceRepository.save(apolice));
-    }*/
+
 	@PutMapping("/{id}")
 	public ResponseEntity<Apolice> put(
 	        @PathVariable Long id,
